614d0867d1636ac4ce956be501b30dca5f0f911f
habibesayrac <habibesayrac@outlook.com>
2024-02-02 21:31:52 +0300

    Add project files.

---
+++ Proje6/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Proje6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Proje6;Integrated Security=True");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            baglanti.Open();
+           SqlDataAdapter da = new SqlDataAdapter("Execute Proje6", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+    }
+}
